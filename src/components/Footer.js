23e74b760a669af726c5667fd009d634cd9ba2ff
goldsnort <dheerajrajput7056@gmail.com>
2022-01-20 11:20:52 +0530

    Footer changes

---
+++ src/components/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './Footer.css';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <div className='waves-bottom'>
+                <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 150" preserveAspectRatio="none" >
+                    <path  d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" 
+                    class="shape-fill" 
+                    fill="#FFFFFF" 
+                    fill-opacity="1">
+                    </path>
+                </svg>
+            </div>
+            <div className='footer'>
+                <div className='footer-list'>
+                    <div className='footer-left'>
+                        <div className='footer-item'><AiIcons.AiFillGithub size={45} /> Github repository</div>
+                        <div className='footer-item'><IoIcons.IoMdContact size={45}/>Contacts</div>
+                    </div>
+                    <div className='footer-title'>BusyGenie</div>
+                    <div className='footer-right'>
+                        <div className='footer-item'><AiIcons.AiOutlineLink size={45} />Linktree</div>
+                        <div className='footer-item'><IoIcons.IoIosChatboxes size={45}/>Feedback</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import './Home.css';
+
 
 
 function Home() {
@@ -10,14 +12,14 @@ function Home() {
                     <p className='home-page-top-desc'>The only productivity app you will ever need</p>
             </div>
             <div className='waves-top'>
-                    <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 150" preserveAspectRatio="none" >
-                        <path  d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" 
-                        class="shape-fill" 
-                        fill="#FFFFFF" 
-                        fill-opacity="1">
-                        </path>
-                    </svg>
-                </div>
+                <svg  data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 150" preserveAspectRatio="none" >
+                    <path  d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" 
+                    class="shape-fill" 
+                    fill="#FFFFFF" 
+                    fill-opacity="1">
+                    </path>
+                </svg>
+            </div>
             <div className='home-page-features'>
                 <div className='features-heading'>
                     FEATURES :
@@ -50,6 +52,36 @@ function Home() {
                     </div>
                 </div>
             </div>
+            <div class="custom-shape-divider-top-1642579492-reverse">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+                </svg>
+            </div>
+            <div className='home-page-dev'>
+                <div className='dev-heading'>
+                    DEVELOPERS :
+                </div>
+                <div className='dev-container'>
+                    <div className='dev-card'>
+                        <img className='dev-photo' alt="dev" src={require("./himshikhar.jpg")}></img>
+                        <p className='dev-name'>Himshikhar Singh</p>
+                    </div>
+                    <div className='dev-card'>
+                        <img className='dev-photo' alt="dev" src={require("./himshikhar.jpg")}></img>
+                        <p className='dev-name'>Dheeraj Kumar</p>
+                    </div>
+                    <div className='dev-card'>
+                        <img className='dev-photo' alt="dev" src={require("./himshikhar.jpg")}></img>
+                        <p className='dev-name'>Siddhant Salve</p>
+                    </div>
+                </div>
+            </div>
+            <div class="custom-shape-divider-top-1642579492">
+                <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                    <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+                </svg>
+            </div>
+            <Footer/>
         </div>
     )
 }
